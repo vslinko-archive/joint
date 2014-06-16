0d9c249494326d75d835c24cb5f1432354dba639
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2014-06-16 15:01:53 +0400

    feature: parse argv

--- joint.gyp
+++ joint.gyp
@@ -9,6 +9,18 @@
                 "src/string.c",
                 "src/tokenizer.c",
                 "src/parser.c"
+            ],
+            "conditions": [
+                ["OS=='mac'", {
+                    "include_dirs": [
+                        "/usr/local/include"
+                    ],
+                    "link_settings": {
+                        "libraries": [
+                            "/usr/local/lib/libargp.a"
+                        ]
+                    }
+                }]
             ]
         }
     ]
--- src/joint.c
+++ src/joint.c
@@ -19,32 +19,99 @@
  * THE SOFTWARE.
  */
 
+#include <argp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include "source_file.h"
 #include "tokenizer.h"
 #include "parser.h"
 
-int main(int argc, const char ** argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s SOURCE_FILE\n", argv[0]);
-        return 1;
+const char * argp_program_version = "joint 0.1";
+const char * argp_program_bug_address = "Vyacheslav Slinko <vyacheslav.slinko@gmail.com>";
+
+static struct argp_option options[] = {
+    {"tokens", 't', 0, OPTION_ARG_OPTIONAL, "Print tokens and exit"},
+    {"ast", 'a', 0, OPTION_ARG_OPTIONAL, "Print AST tree and exit"},
+    {0}
+};
+
+struct arguments {
+    char * source_file_path;
+    bool tokens;
+    bool ast;
+};
+
+static error_t parse_opt(int key, char * arg, struct argp_state * state) {
+    struct arguments * arguments = state->input;
+
+    switch (key) {
+        case 't':
+            arguments->tokens = true;
+            break;
+
+        case 'a':
+            arguments->ast = true;
+            break;
+
+        case ARGP_KEY_NO_ARGS:
+            argp_usage(state);
+            break;
+
+        case ARGP_KEY_ARG:
+            arguments->source_file_path = arg;
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
     }
 
-    joint_source_file_t * source_file = joint_source_file_alloc(argv[1]);
+    return 0;
+}
+
+static struct argp argp = {
+    .options = options,
+    .parser = parse_opt,
+    .args_doc = "SOURCE_FILE",
+    .doc = "The Joint language compiler."
+};
+
+static struct arguments arguments = {
+    .source_file_path = "-",
+    .tokens = false,
+    .ast = true
+};
+
+int main(int argc, char ** argv) {
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    joint_source_file_t * source_file = joint_source_file_alloc(arguments.source_file_path);
 
     if (!joint_source_file_read(source_file)) {
-        fprintf(stderr, "Unable to read file \"%s\"\n", argv[1]);
+        fprintf(stderr, "Unable to read file \"%s\"\n", arguments.source_file_path);
         joint_source_file_free(source_file);
-        return 2;
+        return 1;
     }
 
     joint_tokenizer_t * tokenizer = joint_tokenizer_alloc(source_file);
     joint_tokenizer_tokenize(tokenizer);
 
+    if (arguments.tokens) {
+        joint_tokeinzer_print(tokenizer);
+        joint_tokenizer_free(tokenizer);
+        joint_source_file_free(source_file);
+        return 0;
+    }
+
     joint_parser_t * parser = joint_parser_alloc(tokenizer);
     joint_parser_parse(parser);
 
-    joint_parser_print(parser);
+    if (arguments.ast) {
+        joint_parser_print(parser);
+        joint_parser_free(parser);
+        joint_tokenizer_free(tokenizer);
+        joint_source_file_free(source_file);
+        return 0;
+    }
 
     joint_parser_free(parser);
     joint_tokenizer_free(tokenizer);
